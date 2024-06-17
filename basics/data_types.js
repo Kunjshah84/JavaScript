85ba5f542a03b1b6198c358777f21768d2e773e2
Kunjshah84 <155896950+Kunjshah84@users.noreply.github.com>
2024-06-17 20:34:08 +0000

    data_types

---
+++ basics/data_types.js
@@ -0,0 +1,8 @@
+"use strict"
+//This line is goig to tell the engine that consider the code with the new method of java script
+// alert(4+5)
+
+//one of the imp data type is symbol
+//Symbol=>when we want the unique then we use it and it is mejorly used in react etc...
+console.log(typeof null) //object:
+console.log(typeof undefined);//undefined
---
+++ basics/var_const.js
@@ -0,0 +1,14 @@
+const accountid=555505
+let accountemail="abcd@gmail.com"
+var accountpass="123"
+accountcity="jaipur"
+
+// accountid=3
+// console.log(accountid);
+accountemail="abdd@gmail.com"
+var accountpass="124"
+accountcity="jaipura"
+let kunj;
+console.table([accountemail,accountpass,accountcity,kunj])
+//Here we have two methods to declar the var which is var and the let
+//var is having the higher scope like if we declar the variable in type var in funciton then the scope of the variable is throughout the function ans this is not the case in the let:
