48a25a6296a5b08218eef5793533a0ee2c518d46
Kunjshah84 <155896950+Kunjshah84@users.noreply.github.com>
2024-06-18 06:19:08 +0000

    comparison is done

---
+++ basics/comparisition.js
@@ -0,0 +1,13 @@
+console.log("2">1)
+console.log("02">1)//It is good practice to maitian the same data type to compare:
+console.log("2abs">1)
+console.log(null==0)
+console.log(null>=0)
+console.log(null>0)
+console.log(null<=0)
+//Now the resion behind this is that the work of the == nd the >= etc are different in JS: ans here the NULL is converting into 0 when it compers with the >= etc...
+
+//==== it is going to check the data type also brfore the conversion:
+
+
+
---
+++ basics/operation.js
@@ -0,0 +1,20 @@
+let value=3
+let negval=-value
+console.log(negval)
+
+console.log(2**2)//to power the 2 to 2
+
+let str1="hello"
+let str2="kunj"
+let str3=str1+str2
+console.log(str3)
+console.log("1"+1)
+console.log("1"+2+2)
+console.log("1"+2+2)
+console.log(2+2+"1")
+
+console.log(+true)
+console.log(+"")
+
+let num1,num2,num3
+num1=num2=num3=5+5
